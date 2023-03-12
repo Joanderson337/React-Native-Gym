d8805e439d556dd54d5835d0fd0cff29aa20da7d
Joanderson Silva <jhonny_040996@hotmail.com>
2023-03-12 00:30:38 -0300

    feat: rotas, paginas signUp e signIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,25 @@
-import { VStack, Image, Text, Center, Heading } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
+import { useNavigation } from "@react-navigation/native";
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function SignIn() {
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+  function handleNewAccount() {
+    navigation.navigate('signUp');
+  }
+
   return (
-    <VStack flex={1} bg="gray.700">
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+    <VStack flex={1} px={10}>
       <Image 
         source={BackgroundImg}
         alt="Pessoas treinando"
+        defaultSource={BackgroundImg}
         resizeMode="stretch"
         position="absolute"
       />
@@ -22,14 +33,38 @@ export function SignIn() {
 
       
       <Center>
+
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse a conta
         </Heading>
-        <Input placeholder="E-mail" />
-        <Input placeholder="Senha" />
 
+        <Input 
+          placeholder="E-mail" 
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+
+        <Input 
+          placeholder="Senha" 
+          secureTextEntry
+        />
+
+        <Button title="Acessar" />
       </Center>
 
+      <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tem acesso?
+          </Text>
+      </Center>
+
+      <Button 
+        title="Criar Conta" 
+        variant="outline"
+        onPress={handleNewAccount}
+          />
+
     </VStack>
+    </ScrollView>
   );
 }
